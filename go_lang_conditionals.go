f54b9e162ccf6b51896997d437b95edaa4317225
Josh Harty <josh@Joshs-MacBook-Air.local>
2023-05-09 16:55:31 +0200

    first push

---
+++ go_lang_conditionals.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	isHeistOn := true
+
+	eludedGuards := rand.Intn(100)
+
+	if eludedGuards >= 50 {
+		fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+	} else {
+		isHeistOn = false
+		fmt.Println("Plan a better disguise next time?")
+	}
+
+	fmt.Println("Is Heist On?", isHeistOn)
+
+	openedVault := rand.Intn(100)
+
+	if isHeistOn && openedVault >= 70 {
+		fmt.Println("Grab and GO!")
+	} else if isHeistOn {
+		isHeistOn = false
+		fmt.Println("The vault can't be opened.")
+	}
+
+	leftSafely := rand.Intn(5)
+
+	if isHeistOn {
+		switch leftSafely {
+		case 0:
+			isHeistOn = false
+			fmt.Println("Heist failed!")
+		case 1:
+			isHeistOn = false
+			fmt.Println("Turns out vault doors don't open from the inside...")
+		case 2:
+			isHeistOn = false
+			fmt.Println("The alarm was tripped!")
+		case 3:
+			isHeistOn = false
+			fmt.Println("Caught by security cameras!")
+		default:
+			fmt.Println("Start the getaway car!")
+		}
+	}
+
+	if isHeistOn {
+		amtStolen := 10000 + rand.Intn(1000000)
+		fmt.Printf("Amount stolen: $%d\n", amtStolen)
+	}
+}
